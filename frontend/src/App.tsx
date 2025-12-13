14ad54b31901a8ee9d6286041a2d1236c2b54f0d
Emin14 <emain-agjaev@mail.ru>
2025-12-13 14:37:37 +0300

    fix url

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL || ''
 import './App.css'
 
 type Tour =  {
