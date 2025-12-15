1c25180b80b94a8029dfd1d3d19e7ffe4692bef0
Emin14 <emain-agjaev@mail.ru>
2025-12-15 20:00:21 +0300

    add new10

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -49,7 +49,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
         <p>
-          New
+          New2
         </p>
       </div>
       <p className="read-the-docs">
